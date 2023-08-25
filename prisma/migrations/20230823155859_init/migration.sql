8d0253c74b08e4c218db1c73696548a19d968551
Pedro Braghin <braghindev@gmail.com>
2023-08-25 12:41:33 -0300

    feat: add prisma initial config

---
+++ prisma/migrations/20230823155859_init/migration.sql
@@ -0,0 +1,35 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "phone_number" TEXT NOT NULL,
+    "email_verified" BOOLEAN NOT NULL DEFAULT false,
+    "otp_enabled" BOOLEAN NOT NULL DEFAULT false,
+    "otp_secret" TEXT,
+    "google_id" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Address" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "postal_code" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "neighborhood" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Address_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Address" ADD CONSTRAINT "Address_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/infra/database.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from '@prisma/client';
+import config from '../config';
+
+export const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: config.databaseUrl,
+    },
+  },
+});
+
+export class Database {
+  public prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = prisma;
+  }
+
+  async connect() {
+    await this.prisma.$connect();
+    console.log('Database connected');
+  }
+
+  async disconnect() {
+    this.prisma?.$disconnect();
+  }
+}
