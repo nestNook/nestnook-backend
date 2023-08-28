676dd96e428a4bfb46dfe5ce7833c28d99663a13
Pedro Braghin <braghindev@gmail.com>
2023-08-28 17:46:15 -0300

    feat: add routes mapping

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -1,25 +1,86 @@
-import express, { Application } from 'express';
 import config from '../config';
+import AppModule from '../modules/app.module';
+import express, { Application } from 'express';
+import { Server as HttpServer } from 'http';
+import { Route } from '../common/route.interface';
+import { BaseRouter } from '../common/baseRouter.interface';
 
-class Server {
-  private app: Application;
+export class Server {
+  public app: Application;
+  public server: HttpServer | undefined;
+
   constructor() {
     this.app = express();
   }
 
   start() {
-    this.app.listen(config.port);
+    this.server = this.app.listen(config.port);
     this.middlewares();
     this.routes();
+  }
 
+  stop(cb?: () => void) {
+    if (!this.server) return;
+    this.server.close(cb);
+  }
+
+  middlewares() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  }
+
+  routes() {
+    AppModule.routers.forEach((router) => this.addRouter(router));
     console.log(`Server is running on port ${config.port}`);
   }
 
-  stop() {}
+  addRouter(router: BaseRouter) {
+    const routePrefix = router.routePrefix ?? '/';
+    console.group(`[${routePrefix}]:`);
+    this.app.use(routePrefix, this.mapRoutes(router.routes));
+    console.groupEnd();
+  }
 
-  middlewares() {}
+  private mapRoutes(routes: Route[]) {
+    const router = express.Router();
 
-  routes() {}
+    routes.forEach(({ handler, method, middlewares, path }) => {
+      console.log(`[${method.toLocaleUpperCase()}]: ${path}`);
+
+      switch (method) {
+        case 'get': {
+          router.get(path, ...middlewares, handler);
+          return;
+        }
+
+        case 'post': {
+          router.post(path, ...middlewares, handler);
+          return;
+        }
+
+        case 'put': {
+          router.put(path, ...middlewares, handler);
+          return;
+        }
+
+        case 'patch': {
+          router.patch(path, ...middlewares, handler);
+          return;
+        }
+
+        case 'delete': {
+          router.delete(path, ...middlewares, handler);
+          return;
+        }
+
+        default: {
+          console.log(`Unknown method: ${method}`);
+        }
+      }
+    });
+
+    return router;
+  }
 }
 
-export { Server };
+export default new Server();
