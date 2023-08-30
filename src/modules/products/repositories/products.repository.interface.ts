2cd5f75d76a59cdec816a4cd52a0ccca81c5dab0
victorzix <linkgamesplays@gmail.com>
2023-08-30 20:59:26 -0300

    feat: change return type of createProduct method

--- src/modules/products/repositories/products.repository.interface.ts
+++ src/modules/products/repositories/products.repository.interface.ts
@@ -1,5 +1,6 @@
 import { CreateProductDTO } from "../dtos/create-product.dto";
+import { Product } from "../dtos/product.dto";
 
 export interface ProductsRepositoryInterface {
-  createProduct(createProductData: CreateProductDTO): Promise<CreateProductDTO | null>;
+  createProduct(createProductData: CreateProductDTO): Promise<Product | null>;
 }
--- src/modules/products/repositories/products.repository.ts
+++ src/modules/products/repositories/products.repository.ts
@@ -1,9 +1,10 @@
 import { prisma } from '../../../infra/database';
 import { CreateProductDTO } from '../dtos/create-product.dto';
+import { Product } from '../dtos/product.dto';
 import { ProductsRepositoryInterface } from './products.repository.interface';
 
 export class ProductsRepository implements ProductsRepositoryInterface {
-  async createProduct(productDto: CreateProductDTO): Promise<CreateProductDTO | null> {
+  async createProduct(productDto: CreateProductDTO): Promise<Product | null> {
     const product = await prisma.product.create({data: productDto})
     return product
   }
