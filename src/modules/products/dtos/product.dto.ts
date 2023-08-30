e0ea47eeb71d900bbba26cbe82f6fc54ba1f2339
victorzix <linkgamesplays@gmail.com>
2023-08-30 19:55:43 -0300

    feat: create productDTO

---
+++ src/modules/products/dtos/product.dto.ts
@@ -0,0 +1,14 @@
+export interface Product {
+  id: string,
+  sku: string;
+  name: string;
+  description: string;
+  category: string;
+  price: bigint;
+  quantity: number;
+  currency: string;
+  sector_id: string;
+  fabricator_id: string;
+  created_at: Date,
+  updated_at: Date
+}
