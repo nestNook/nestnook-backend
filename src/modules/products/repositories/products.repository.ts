2663cee1c5b309664213264ee0e8d4f472249ff2
victorzix <linkgamesplays@gmail.com>
2023-08-30 13:13:21 -0300

    chore: install jest types

--- src/modules/products/repositories/products.repository.ts
+++ src/modules/products/repositories/products.repository.ts
@@ -1,3 +1,10 @@
+import { prisma } from '../../../infra/database';
+import { CreateProductDTO } from '../dtos/create-product.dto';
 import { ProductsRepositoryInterface } from './products.repository.interface';
 
-export class ProductsRepository implements ProductsRepositoryInterface {}
+export class ProductsRepository implements ProductsRepositoryInterface {
+  async createProduct(productDto: CreateProductDTO): Promise<CreateProductDTO | null> {
+    const product = await prisma.product.create({data: productDto})
+    return product
+  }
+}
