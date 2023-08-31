f0d3a1572c56e4883c128f2c7d3d8781c2755fe4
Pedro Braghin <braghindev@gmail.com>
2023-08-31 08:28:46 -0300

    chore: update coverageThreshold

--- jest.config.ts
+++ jest.config.ts
@@ -46,10 +46,10 @@ const config: Config = {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      statements: 90,
-      branches: 90,
-      functions: 90,
-      lines: 90,
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
     },
   },
 
