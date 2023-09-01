d719f84c6e25ecda09d0b4e929b13b7dd8ef488d
Pedro Braghin <braghindev@gmail.com>
2023-09-01 17:37:08 -0300

    feat: add jwt variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,12 @@ import 'dotenv/config';
 
 export class Config {
   public readonly port: string | number = process.env.PORT || 8081;
-  public readonly databaseUrl: string = String(process.env.DATABASE_URL);
+  public readonly databaseUrl: string = process.env.DATABASE_URL;
+  public readonly apiPrefix: string = process.env.API_PREFIX;
+  public readonly jwtSecret: string = process.env.JWT_SECRET;
+  public readonly jwtExpiresIn: string = process.env.JWT_EXPIRES_IN;
+  public readonly refreshTokenExpiresIn: string =
+    process.env.REFRESH_TOKEN_EXPIRES_IN;
 }
 
 export default new Config();
