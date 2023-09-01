b8db5ad517f440b7253a824e63b0f5f5264ab7a0
Victor <linkgamesplays@gmail.com>
2023-09-01 08:41:56 -0300

    feat: create address interfaces

---
+++ src/modules/address/dtos/address.dto.ts
@@ -0,0 +1,12 @@
+export interface Address {
+  id: string;
+  user_id: string;
+  postal_code: string;
+  country: string;
+  street: string;
+  state: string;
+  city: string;
+  neighborhood: string;
+  created_at: Date;
+  updated_at: Date;
+}
---
+++ src/modules/address/dtos/index.ts
@@ -0,0 +1,2 @@
+export * from './address.dto'
+export * from './create-address.dto'
