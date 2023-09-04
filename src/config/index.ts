54572f74f4a5e51984e506f6b30bd1b1895d412c
Pedro Braghin <braghindev@gmail.com>
2023-09-04 14:39:04 -0300

    chore: add mongodb url

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 export class Config {
   public readonly port: string | number = process.env.PORT || 8081;
   public readonly databaseUrl: string = process.env.DATABASE_URL;
+  public readonly mongodbUrl: string = process.env.MONGODB_URL;
   public readonly apiPrefix: string = process.env.API_PREFIX;
   public readonly jwtSecret: string = process.env.JWT_SECRET;
   public readonly jwtExpiresIn: string = process.env.JWT_EXPIRES_IN;
