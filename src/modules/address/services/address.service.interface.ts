244fabaa22ee42d874c3000100a568e946acd5f4
Pedro Braghin <braghindev@gmail.com>
2023-09-08 09:37:38 -0300

    feat: add crud to address module

--- src/modules/address/services/address.service.interface.ts
+++ src/modules/address/services/address.service.interface.ts
@@ -1,5 +1,15 @@
-import { CreateAddressDTO, Address } from "../dtos";
+import { CreateAddressDTO, Address, UpdateAddressDTO } from '../dtos';
 
-export interface AddressServiceInterface { 
-  createAddress(dto: CreateAddressDTO): Promise<Address>
- }
+export interface AddressServiceInterface {
+  createAddress(dto: CreateAddressDTO): Promise<Address>;
+  getAddressById(id: string, userId: string): Promise<Address>;
+  getUserAddresses(userId: string): Promise<Address[]>;
+  updateAddress(
+    id: string,
+    userId: string,
+    dto: UpdateAddressDTO
+  ): Promise<Address>;
+  deleteAddress(id: string, userId: string): Promise<Address>;
+  deleteUserAddresses(userId: string): Promise<Address[]>;
+  deleteFabricatorAddresses(userId: string): Promise<Address[]>;
+}
