e852ca28e75b04c6ea8d95b168efa4484da25495
Pedro Braghin <braghindev@gmail.com>
2023-09-08 14:10:51 -0300

    feat: add sign in route

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -3,12 +3,23 @@ import { SignInDTO } from '../dtos/sign-in.dto';
 import { AuthServiceInterface } from '../services/auth.service.interface';
 import { AuthControllerInterface } from './auth.controller.interface';
 import { Controller } from '@common/controller.decorator';
+import { Request, Response } from 'express';
+import cookieUtils from '@utils/cookie-utils';
 
 @Controller
 export class AuthController implements AuthControllerInterface {
   constructor(private readonly authService: AuthServiceInterface) {}
 
-  async signIn(dto: SignInDTO): Promise<SessionDTO> {
-    throw new Error('Method not implemented.');
+  async signIn(req: Request, res: Response): Promise<Response> {
+    const dto: SignInDTO = req.body;
+    const session: SessionDTO = await this.authService.login(dto);
+
+    cookieUtils.accessToken(session.access_token, res);
+    cookieUtils.refreshToken(session.refresh_token, res);
+
+    return res.status(200).json({
+      status: 'success',
+      data: session,
+    });
   }
 }
--- src/modules/auth/services/auth.service.interface.ts
+++ src/modules/auth/services/auth.service.interface.ts
@@ -1,1 +1,5 @@
-export interface AuthServiceInterface {}
+import { SessionDTO } from '@@types/session.dto';
+import { SignInDTO } from '../dtos/sign-in.dto';
+export interface AuthServiceInterface {
+  login(SignInDTO: SignInDTO): Promise<SessionDTO>;
+}
