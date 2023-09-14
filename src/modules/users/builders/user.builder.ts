ef0a78c7f18812606af67834139cdc2b4e24cd07
Pedro Braghin <braghindev@gmail.com>
2023-09-14 12:59:16 -0300

    refactor: update folder name

--- src/modules/users/builders/user.builder.ts
+++ src/modules/users/builders/user.builder.ts
@@ -1,11 +1,12 @@
 import { GetUserDTO, User } from '../dtos';
 
 export class UserBuilder {
-  static publicUser({ email, id, name }: User): GetUserDTO {
+  static publicUser({ email, id, name, role }: User): GetUserDTO {
     const publicUser: GetUserDTO = {
       email,
       id,
       name,
+      role,
     };
 
     return publicUser;
