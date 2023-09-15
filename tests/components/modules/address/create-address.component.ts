c5899660d262de050447e6080d681be61eae4af6
Pedro Braghin <braghindev@gmail.com>
2023-09-15 10:12:38 -0300

    refactor: improve session mock

--- tests/components/modules/address/create-address.component.ts
+++ tests/components/modules/address/create-address.component.ts
@@ -1,9 +1,7 @@
 import { AddressRepository } from '@modules/address/repositories/address.repository';
-import { userMock } from '@test/units/modules/users/mocks/users-mock';
-import { AuthService } from '@modules/auth/services/auth.service';
+import request, { type Response } from 'supertest';
 import { server } from '@test/components/setup';
 import { When, Then } from '@cucumber/cucumber';
-import request, { Response } from 'supertest';
 import * as sinon from 'sinon';
 import assert from 'assert';
 
@@ -15,31 +13,22 @@ import {
 import {
   accessToken,
   refreshToken,
-  sessionMock,
 } from '@test/units/modules/sessions/mocks/sessions-mock';
-import { AuthRepository } from '@modules/auth/repositories/auth.repository';
+import { MockSession } from '@test/__mocks__/sessions-mocks';
 
 let response: Response;
 
 When('a user send a post request to {string}', async function (url: string) {
   const createUserRepositoryStub = sinon.stub(
     AddressRepository.prototype,
-    'createAddress'
+    'createAddress',
   );
 
-  const findUserByIdRepositoryStub = sinon.stub(
-    AuthRepository.prototype,
-    'findUserById'
-  );
+  MockSession.mockSession();
 
-  const createSessionServiceStub = sinon.stub(
-    AuthService.prototype,
-    'findSessionById'
+  createUserRepositoryStub.callsFake(
+    async () => await Promise.resolve(addressMock),
   );
-
-  createSessionServiceStub.callsFake(() => Promise.resolve(sessionMock));
-  createUserRepositoryStub.callsFake(() => Promise.resolve(addressMock));
-  findUserByIdRepositoryStub.callsFake(() => Promise.resolve(userMock));
 
   response = await request(server.app)
     .post(url)
@@ -51,8 +40,7 @@ When('a user send a post request to {string}', async function (url: string) {
     .send(createAddressMock);
 
   createUserRepositoryStub.restore();
-  createSessionServiceStub.restore();
-  findUserByIdRepositoryStub.restore();
+  MockSession.restoreSessionMock();
 });
 
 Then('the response status should be {int}', function (status: number) {
@@ -66,5 +54,5 @@ Then(
     assert(response.body.data.id);
     assert(response.body.data.created_at);
     assert(response.body.data.updated_at);
-  }
+  },
 );
